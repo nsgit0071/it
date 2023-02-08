fc6d97f3907211222f5c8c9cff64f8a62f9d67de
Nurmuhammad Qosimov <124587611+nsgit0071@users.noreply.github.com>
2023-02-08 14:31:33 +0500

    first commit

---
+++ resources/views/frontend/layout/frontend_script.blade.php
@@ -0,0 +1,14 @@
+<script src="{{ asset('frontend_assets/assets/vendors/js/glightbox.min.js')}}"></script>
+
+<script type="text/javascript">
+    const lightbox = GLightbox({
+        'touchNavigation': true,
+        'href': 'https://www.youtube.com/watch?v=J9lS14nM1xg',
+        'type': 'video',
+        'source': 'youtube', //vimeo, youtube or local
+        'width': 900,
+        'autoPlayVideos': 'true',
+    });
+
+</script>
+<script src="{{ asset('frontend_assets/assets/js/bootstrap.bundle.min.js') }} "></script>
